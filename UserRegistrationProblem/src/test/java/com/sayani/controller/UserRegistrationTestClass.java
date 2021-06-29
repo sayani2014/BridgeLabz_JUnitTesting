3872487822d384a92607a5376ea450be6f761ac4
Sayani Koley <sayanikoley8@gmail.com>
2021-06-29 12:22:13 +0530

    Ability to test phone number using JUnit concept

--- UserRegistrationProblem/src/test/java/com/sayani/controller/UserRegistrationTestClass.java
+++ UserRegistrationProblem/src/test/java/com/sayani/controller/UserRegistrationTestClass.java
@@ -44,4 +44,21 @@ public class UserRegistrationTestClass {
         boolean result = userImpl.validateName("koley");
         Assertions.assertFalse(result);
     }
+
+    /**
+     * Purpose : Validate Phone Number with 0|91 in the beginning, followed by a white space, followed by 10 digit number.
+     * Return true if the string value passes else return false
+     */
+
+    @Test
+    public void givenphonenumber_Proper_ShouldReturnTrue() {
+        boolean result = userImpl.validatePhoneNumber("0 7894561230");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenphonenumber_Proper_ShouldReturnFalse() {
+        boolean result = userImpl.validatePhoneNumber("01 59456123");
+        Assertions.assertFalse(result);
+    }
 }
